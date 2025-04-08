af1efa65b82a8db1e91767782edb3f930889d68b
mox692 <moymoymox@gmail.com>
2025-04-08 16:37:59 +0900

    update bench

--- examples/fs_bench_io_uring.rs
+++ examples/fs_bench_io_uring.rs
@@ -3,43 +3,42 @@
 use std::{hint::black_box, time::Instant};
 use tokio::{
     fs::{read, read3, OpenOptions},
-    task::JoinSet,
+    task::{yield_now, JoinSet},
 };
 
 fn rt() -> tokio::runtime::Runtime {
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
-        .worker_threads(8)
+        // .worker_threads(1)
         .build()
         .unwrap()
 }
 
 fn main() {
     let rt = rt();
-    let num_files = 10;
+    let num_files = 1000;
     let mut dur = None;
-    let iters = 1;
+    let iters = 10;
+
+    let mut set = JoinSet::new();
 
     rt.block_on(async {
-        let mut set = JoinSet::new();
         let start = Instant::now();
         for _ in 0..iters {
             for i in 1..=num_files {
                 set.spawn(async move {
                     let path = format!("/home/mox692/work/tokio/test_file/{i}.txt");
 
-                    let file = OpenOptions::new().read(true).open3(&path).await.unwrap();
-                    black_box(file);
+                    // let file = OpenOptions::new().read(true).open3(&path).await.unwrap();
+                    // black_box(file);
 
-                    // let res = read3(&path).await.unwrap();
-                    // black_box(res);
+                    let res = read3(&path).await.unwrap();
+                    black_box(res);
                 });
             }
         }
 
-        while let Some(h) = set.join_next().await {
-            h.unwrap()
-        }
+        while let Some(Ok(_)) = set.join_next().await {}
 
         dur = Some(start.elapsed())
     });
--- examples/fs_bench_spawn_blocking.rs
+++ examples/fs_bench_spawn_blocking.rs
@@ -1,13 +1,15 @@
+/// cargo build --release --package examples --example fs_bench_io_uring
+/// ./target/release/examples/fs_bench_io_uring
 use std::{hint::black_box, time::Instant};
 use tokio::{
     fs::{read, read3, OpenOptions},
-    task::JoinSet,
+    task::{yield_now, JoinSet},
 };
 
 fn rt() -> tokio::runtime::Runtime {
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
-        .worker_threads(8)
+        // .worker_threads(1)
         .build()
         .unwrap()
 }
@@ -18,25 +20,26 @@ fn main() {
     let mut dur = None;
     let iters = 10;
 
+    let mut set = JoinSet::new();
+
     rt.block_on(async {
-        let mut set = JoinSet::new();
         let start = Instant::now();
         for _ in 0..iters {
             for i in 1..=num_files {
                 set.spawn(async move {
                     let path = format!("/home/mox692/work/tokio/test_file/{i}.txt");
 
-                    let file = OpenOptions::new().read(true).open(&path).await.unwrap();
-                    black_box(file);
+                    // let file = OpenOptions::new().read(true).open(&path).await.unwrap();
+                    // black_box(file);
 
-                    // let res = read(&path).await.unwrap();
-                    // black_box(res);
+                    let res = read(&path).await.unwrap();
+                    black_box(res);
                 });
             }
         }
 
         while let Some(h) = set.join_next().await {
-            h.unwrap()
+            h.unwrap();
         }
 
         dur = Some(start.elapsed())
