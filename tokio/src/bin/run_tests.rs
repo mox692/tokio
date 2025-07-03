697b7d696b9dc08738cf3c08bfa9c9a612da89a8
mox692 <moymoymox@gmail.com>
2025-07-03 17:11:17 +0900

    simlify run app

--- tokio/src/bin/run_tests.rs
+++ tokio/src/bin/run_tests.rs
@@ -2,26 +2,28 @@ use std::process::exit;
 use std::process::Command;
 
 fn main() {
-    let project_root = env!("CARGO_MANIFEST_DIR");
+    // let project_root = env!("CARGO_MANIFEST_DIR");
 
-    let status = Command::new("cargo")
-        .arg("test")
-        .arg("-p")
-        .arg("tokio")
-        .arg("--features")
-        .arg("full")
-        .current_dir(project_root)
-        .status()
-        .unwrap_or_else(|e| {
-            eprintln!("failed to execute cargo test: {}", e);
-            exit(1);
-        });
+    // let status = Command::new("cargo")
+    //     .arg("test")
+    //     .arg("-p")
+    //     .arg("tokio")
+    //     .arg("--features")
+    //     .arg("full")
+    //     .current_dir(project_root)
+    //     .status()
+    //     .unwrap_or_else(|e| {
+    //         eprintln!("failed to execute cargo test: {}", e);
+    //         exit(1);
+    //     });
 
-    match status.code() {
-        Some(code) => exit(code),
-        None => {
-            eprintln!("process terminated by signal");
-            exit(1);
-        }
-    }
+    // match status.code() {
+    //     Some(code) => exit(code),
+    //     None => {
+    //         eprintln!("process terminated by signal");
+    //         exit(1);
+    //     }
+    // }
+
+    println!("hello, world!");
 }
